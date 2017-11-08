c1f3744d385214e09e407d04e712b5c6834700b5
Qoole <gmod2007@gmail.com>
2017-11-08 20:14:29 +0300

    RABOTAET!!!!

--- test/app/controllers/patient.js
+++ test/app/controllers/patient.js
@@ -12,18 +12,18 @@ chai.use(chaihttp);
 describe('patient getById test', function() {
     this.timeout(15000);
 
-    // it('no query params', function(done) {
-    //     chai.request(app).get('/patient/getById').end(function(err, res) {
-    //         res.should.has.status(404);
-    //         res.text.should.equal('Can not find id in query parameters!');
-    //         done();
-    //     });
-    // });
+    it('no query params', function() {
+        chai.request(app).get('/patient/getById').end(function(err, res) {
+            res.should.has.status(404);
+            res.text.should.equal('Can not find id in query parameters!');
+            done();
+        });
+    });
 
-      it('find succesfull', function() {
-          chai.request(app).get('/patient/getById?id=1').end(function(err, res) {
-              res.should.has.status(200);
-              done();
-          });
-      });
+    it('find succesfull', function() {
+        chai.request(app).get('/patient/getById?id=1').end(function(err, res) {
+            res.should.has.status(200);
+            done();
+        });
+    });
 });
